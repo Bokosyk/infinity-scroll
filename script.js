9c6f30f77ad363cd25ad54ead1cb81a1fd0bd21b
Leandro Parrado <lparrdev@gmail.com>
2022-02-16 21:58:50 -0500

    getPhotos function created, loading API data into console for testing purposes

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const count = 10;
 const apiKey = 'API';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Get photos from Unsplash API
+async function getPhotos() {
+    try{
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getPhotos();
